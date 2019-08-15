8e1607352cd79e50d05880e5a9e73792ac8de0c2
nastioushka <nastia@nastia.nastia>
2019-08-15 20:22:10 +0200

    dynamic nav first part

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
-include 'master.phtml'; 
+$headings = [];
+array_push($headings, ["content_index" => "presentation"]);
+
+function print_nav($headings) {
+  for ($i=0; $i < sizeof($headings); $i++) {
+    foreach ($headings[$i] as $key => $value) {
+      echo '<li><a href="#">'.$value.'</a></li>';
+    }
+  }
+};
+
+include 'master.phtml';
